7973617cabdf04bda384d2ce76f0d95391b4c8df
paulinha <paulinha@MacBook-Pro-de-seguranca.local>
2022-12-14 19:44:29 -0300

    Resolução do Requisito 1 - corrigido

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/" component={ Login } />
             <Route path="/search" component={ Search } />
-            <Route path="/album/id:" component={ Album } />
+            <Route path="/album/:id" component={ Album } />
             <Route path="/favorites" component={ Favorites } />
-            <Route path="/profile" component={ Profile } />
             <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route path="/profile" component={ Profile } />
             <Route path="*" component={ NotFound } />
           </Switch>
         </BrowserRouter>
