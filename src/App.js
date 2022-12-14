40e3456238873e9e14a1b1c50692c64b94022c77
paulinha <paulinha@MacBook-Pro-de-seguranca.local>
2022-12-13 22:23:02 -0300

    Resolução do Requisito 1 com teste

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <main>
+            <Login />
+            <Search />
+            <Album />
+            <Favorites />
+            <Profile />
+            <ProfileEdit />
+            <NotFound />
+          </main>
+        </BrowserRouter>
+
+      </>
+    );
   }
 }
 
 export default App;
-
-vcvcvcvcvcv
